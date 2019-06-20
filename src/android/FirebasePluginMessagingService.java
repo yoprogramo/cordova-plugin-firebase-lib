f0b3dd82cebb77a16ab42c784c2216168df3d6fe
Jose Antonio Espinosa <espinosa@biblioeteca.com>
2019-06-20 11:46:11 +0200

    To remove androidx

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import androidx.core.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.app.Notification;
 import android.text.TextUtils;
